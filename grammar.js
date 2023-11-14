e8dfd2b2aa060e46f294c299d93a21757c55f479
atusy <30277794+atusy@users.noreply.github.com>
2023-11-14 20:07:57 +0900

    feat: support path starting without /

--- grammar.js
+++ grammar.js
@@ -9,22 +9,27 @@ module.exports = grammar({
     uri: $ => seq(
       $.scheme,
       ':',
-      optional($.authority),
-      optional($.path),
+      choice(
+        prec(2, seq($.authority, $.path)), // e.g., https://example.com/foo
+        $.authority, // e.g., https://example.com
+        $.path, // e.g., file:///home
+      ),
       optional(seq('?', $.query)),
       optional(seq('#', $.fragment)),
     ),
     authority: $ => seq(
       '//',
       optional($.userinfo),
-      $.host,
-      optional(seq(':', $.port)),
+      choice(
+        prec(2, seq($.host, ':', $.port)),
+        $.host,
+      ),
     ),
     scheme: $ => /[a-zA-Z][a-zA-Z0-9+.-]*/,
     userinfo: $ => /[^\n@]+@/, // TODO: should separate @
     host: $ => /[^\n@:/]*/, // TODO: should support :
     port: $ => /\d+/,
-    path: $ => /\/([^/][^\n?#]*)?/, // NOTE: avoid matching ^//, or authority becomes a part of path
+    path: $ => seq(/[^\n:]/, /([^\n?#]*)?/), // TODO: should support ^:
     query: $ => /[^\n#]*/,
     fragment: $ => /[^\n]*/,
   }
